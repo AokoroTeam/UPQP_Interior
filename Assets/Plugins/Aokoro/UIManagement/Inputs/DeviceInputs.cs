291e49d8dfd3bf0bbfdb66deb248b79e98b20a20
Lupeni <lupeni.tuikalepa@gmail.com>
2022-02-22 17:39:36 +0100

    slice view partie 3

---
+++ Assets/Plugins/Aokoro/UIManagement/Inputs/DeviceInputs.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Editor;
+
+namespace Aokoro.UIManagement.InputRepresentation
+{
+    [CreateAssetMenu(menuName = "Aokoro/UI/Inputs/DeviceInputs")]
+    public class DeviceInputs : ScriptableObject
+    {
+    }
+}
